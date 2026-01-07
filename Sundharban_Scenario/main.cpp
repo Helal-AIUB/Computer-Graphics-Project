5f8e0fdc8b5696e77a3f3ff2d03f2f668b6513de
Jarif99 <jarifahamed80@gmail.com>
2026-01-08 00:42:17 +0600

    Update code

--- Sundharban_Scenario/main.cpp
+++ Sundharban_Scenario/main.cpp
@@ -3,6 +3,61 @@
 #include <cstdlib>
 
 
+//night
+bool isNight = false;
+
+//rain
+
+bool isRain = false;
+const int RAIN_DROPS = 300;
+float rainX[RAIN_DROPS], rainY[RAIN_DROPS];
+
+
+
+// Window dimensions
+int width = 1000;
+int height = 600;
+
+// Boat positions
+float boatX1 = 500, boatX2 = 700, boatX3 = 900, boatX4 = 1100;
+
+// Cloud positions
+float cloudX1 = 400, cloudX2 = 700, cloudX3 = 1000;
+
+
+// Tiger position
+float tigerX = -600;
+
+// Deer position
+float deerX = -600;
+
+
+
+// snak3
+
+float snakeX = -width / 2 - 300;  // sanake bam theke cholbe
+float snakeY = -height / 4 - 100; // nodite position
+
+const int numSegments = 15;
+const float segmentLength = 7.0f;
+const float bodyRadius = 4.0f;
+
+float waveAmplitude = 7.0f;
+float waveFrequency = 0.4f;
+float timeValue = 0.0f;
+
+
+// snake
+void drawSundarbanCircle(float cx, float cy, float r, int num_segments) {
+    glBegin(GL_TRIANGLE_FAN);
+    for (int i = 0; i <= num_segments; i++) {
+        float theta = 2.0f * 3.1416f * i / num_segments;
+        float x = r * cosf(theta);
+        float y = r * sinf(theta);
+        glVertex2f(cx + x, cy + y);
+    }
+    glEnd();
+}
 
 // Main
 int main(int argc, char** argv) {
